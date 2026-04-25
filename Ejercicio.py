d7c7f2a19b93b4721855196f35f3ed3d846aa342
Ronqui <warcamilode@gmail.com>
2026-04-25 18:38:57 -0500

    Create Ejercicio.py

---
+++ Ejercicio.py
@@ -0,0 +1,22 @@
+from rich.table import Table
+from rich.console import Console
+
+archivos = ["documento.dox", "documento.pdf", "documento.xlsx"]
+
+t = Table(title= "extenciones")
+t.add_column("Archivo")
+t.add_column("Nombre")
+t.add_column("Extension")
+
+for i in archivos:
+    nombre = i[:i.index('.')]
+    extension = i[i.index('.')+1:]
+
+    t.add_row(
+        f"{i}",
+        f"({nombre})",
+        f"({extension})"
+    )
+
+Console().print(t)
+#hola mundo
